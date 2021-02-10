05a9a86b00fa8fc034f994428a1ce0b3a87e0e5c
kerbychuable <kerbychua@gmail.com>
2021-02-11 02:23:06 +0800

    Polish ARP Scanning
    - Pretty print ARP results
    - User input IP range for scanning

--- main.py
+++ main.py
@@ -5,9 +5,23 @@
 #   CHUA, Kerby
 #   LOPEZ, Earth
 
-from scapy.all import ARP, Ether, srp
+import os
+from scapy.all import *
+from tabulate import tabulate
+
+def clrscr():
+  if os.name == 'nt': # Windows OS
+    os.system('cls')
+  else: # Non-Windows
+    os.system('clear')
 
 if __name__ == "__main__":
-  target_ip = "192.168.1.1/24"
-  ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target_ip), timeout=5)
-  ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%"))
+  target_ip = input("Input IP range for scanning (e.g. 192.168.0.1/24): ")
+  ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target_ip), timeout=5) # store answered and unanswered packets
+  clrscr()
+  clients = []
+  for s, r in ans:
+    clients.append({'IP Address': r.psrc, 'MAC Address': r.hwsrc})
+
+  print(tabulate(clients, headers="keys", tablefmt="psql"))
+  os.system('pause')

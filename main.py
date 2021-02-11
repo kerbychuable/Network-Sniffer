0089b189a26b482566861c31d2d12daeb934e7e9
kerbychuable <kerbychua@gmail.com>
2021-02-11 22:36:16 +0800

    Convert to args based

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 #   LOPEZ, Earth
 
 import os
+import argparse
+from argparse import RawTextHelpFormatter
 from scapy.all import *
 from tabulate import tabulate
 
@@ -15,13 +17,33 @@ def clrscr():
   else: # Non-Windows
     os.system('clear')
 
-if __name__ == "__main__":
-  target_ip = input("Input IP range for scanning (e.g. 192.168.0.1/24): ")
-  ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target_ip), timeout=5) # store answered and unanswered packets
-  clrscr()
-  clients = []
-  for s, r in ans:
-    clients.append({'IP Address': r.psrc, 'MAC Address': r.hwsrc})
+def main(argv):
+  parser = argparse.ArgumentParser(prog="NetSniff", formatter_class=RawTextHelpFormatter, description=""
+                                  + "ARP Scan: 'sudo python3 main.py -arp -ip [IP subnetwork]'\n"
+                                  + ""
+                                  + "")
 
-  print(tabulate(clients, headers="keys", tablefmt="psql"))
-  os.system('pause')
+  arp_scan = parser.add_argument_group('ARP Scanning')
+  arp_scan.add_argument('-arp', '--arp-scan', action="store_true", help="Scan Address Resolution Protocol to see connected devices to network")
+  arp_scan.add_argument('-ip', default="", help="Target IP subnetwork")
+
+  args = parser.parse_args()
+  if("-arp" in argv) or ("--arp-scan" in argv):
+    # target_ip = input("Input IP range for scanning (e.g. 192.168.0.1/24): ")
+    # ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target_ip), timeout=5) # store answered and unanswered packets
+    if args.ip == "":
+      print("Please specify target IP subnetwork")
+      print("usage: sudo python3 main.py -arp -ip [IP subnetwork]")
+      sys.exit(0)
+    else:
+      ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=args.ip), timeout=5) # store answered and unanswered packets
+      if not ans: # no devices found
+        print("ARP scan did not receive any response")
+      else:
+        clients = []
+        for s, r in ans:
+          clients.append({'IP Address': r.psrc, 'MAC Address': r.hwsrc})
+        print(tabulate(clients, headers="keys", tablefmt="psql"))
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
